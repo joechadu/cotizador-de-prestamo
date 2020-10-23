564dca274e96d6bd614561b99b915ed10015ea99
Joe <chapiamadurand15@gmail.com>
2020-10-22 19:34:17 -0500

    Firts Program REACT :)

---
+++ src/componentes/Resultado.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Resultado = ({cantidad, total, plazo}) =>(
+   <div className="u-full-width resultado">
+       
+        <h2>Resumen</h2>
+        <p>La cantidad solicitada es: $ {cantidad}</p>
+        <p>A pagar en: {plazo} meses</p>
+        <p>Su pago mensual es de :$ {(total / plazo).toFixed(2)}</p>
+        <p>total a pagar: ${(total).toFixed(2)}</p>
+   </div>
+);
+ 
+export default Resultado ;
